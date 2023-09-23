dcda1fa960971cd770b8e5852d50cadfb9074808
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-09-23 15:18:32 +0300

    JWT Token

--- WebAPI_JWT/Controllers/AuthController.cs
+++ WebAPI_JWT/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebAPI_JWT.DTOs;
 
 namespace WebAPI_JWT.Controllers
@@ -10,12 +14,12 @@ namespace WebAPI_JWT.Controllers
     public class AuthController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        public AuthController(UserManager<IdentityUser> userManager,IConfiguration configuration)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
+            _configuration = configuration;
         }
 
 
@@ -70,7 +74,7 @@ namespace WebAPI_JWT.Controllers
                 if (result)
                 {
                     //token
-                    var token = CreateToken();
+                    var token = CreateToken(user);
                     return Ok(token);
                 }
 
@@ -83,12 +87,44 @@ namespace WebAPI_JWT.Controllers
         }
 
 
-        public string CreateToken()
+        public string CreateToken(IdentityUser user)
         {
             //Kullanıcı Bilgilerine Ait Token Oluşturma
 
             //Claims
-            return "test";
+            var claims = new List<Claim>()
+            {
+                //Subject : Claim JWT'nin subject alanını temsil eder.
+                new Claim(JwtRegisteredClaimNames.Sub,user.Email),
+
+                //JWT Id : Claim JTI alanını temsil eder. Rastgele bir Guid değeri içerir. JWTI alanı o JWT'nin benzersiz bir kimliğini temsil etmektedir. JWT tekrar kullanılabilmesi ve güvenliği için geçerlidir.
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+
+                //Name Identifier : Claim JWT'nin nameidentifier alanını temsil eder ve kulalanıcının kimlik bilgisini içerir.
+                new Claim(ClaimTypes.NameIdentifier,user.Id)
+            };
+
+            //Key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+
+            //Credentials
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //Expire
+            var expire = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JWT:ExpireDays"]));
+
+            //Token
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                claims:claims,
+                expires:expire,
+                signingCredentials:cred
+                );
+
+            var result=new JwtSecurityTokenHandler().WriteToken(token);
+
+            return result;
         }
 
 
